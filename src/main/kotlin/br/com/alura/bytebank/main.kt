7cd248f09a9376854b5959d596e43542f0aa4a48
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-09-28 00:53:14 -0300

    Tratando Exceptions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,16 +1,43 @@
 import br.com.alura.bytebank.modelo.Funcionario
+import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
-//    reproduzindo ArithmeticException
-//    10/0
+
+    val entrada = "1.0"
+
+    val valorConvertido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorConvertido != null) {
+        valorConvertido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
 
 fun funcao1(){
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastException foi capturada")
+    }
     println("fim funcao1")
 }
 
@@ -18,9 +45,9 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5){
         println(i)
-//        reproduzindo class cast exception
-//        val falsoFuncionario = Any()
-//        falsoFuncionario as Funcionario
+
+        val falsoFuncionario = Any()
+        falsoFuncionario as Funcionario
     }
     println("fim funcao2")
 }
