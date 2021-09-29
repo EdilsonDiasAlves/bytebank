b57d180a0458938c4fb17e0b0bf12f225a799067
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-09-29 11:31:17 -0300

    Extraindo metodo para testagem de pilha

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,31 +1,8 @@
-import br.com.alura.bytebank.modelo.Funcionario
+import br.com.alura.bytebank.teste.funcao1
 import br.com.alura.bytebank.teste.testaComportamentosConta
 
 fun main() {
     println("início main")
-    testaComportamentosConta()
-//    funcao1()
+    funcao1()
     println("fim main")
-}
-
-fun funcao1(){
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: ClassCastException) {
-        e.printStackTrace()
-        println("ClassCastException foi capturada")
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-    for (i in 1..5){
-        println(i)
-
-        val falsoFuncionario = Any()
-        throw NumberFormatException()
-    }
-    println("fim funcao2")
 }
