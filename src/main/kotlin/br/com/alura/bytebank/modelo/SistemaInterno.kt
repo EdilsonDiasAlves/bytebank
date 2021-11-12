32cebcbd27b49bba533999af0c1f3cceee99d896
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-11-12 17:13:55 -0300

    Implementando Higher Order Functions

--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,9 +2,10 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entra(funcionario: Autenticavel, senha: Int) {
+    fun entra(funcionario: Autenticavel, senha: Int, autenticado: () -> Unit = {}) {
         if(funcionario.autentica(senha)){
             println("Bem vindo ao Bytebank")
+            autenticado()
         } else {
             println("Falha na autenticação")
         }
